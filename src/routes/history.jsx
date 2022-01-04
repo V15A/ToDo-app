b9609c29f2232c174bc1011242edf5c1c88652ba
Visa Keskinen <visa.keskinen@gmail.com>
2022-01-04 12:19:41 +0200

    now renders list of done tasks similar to App.js

--- src/routes/history.jsx
+++ src/routes/history.jsx
@@ -1,12 +1,85 @@
 import Sidebar from "../sidebar";
+import CreateList from "../taskList";
+import React, { useState, useEffect } from "react";
 
 export default function History() {
-  return (
-    <main className="history">
-      <Sidebar />
-      <h1>History view</h1>
-      <p>the history is going to be listed here</p>
-      <h1>HISTORY</h1>
-    </main>
-  );
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch("http://localhost:3010/tasks");
+        const json = await res.json();
+        setData(json);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getData();
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  if (isLoading) {
+    return (
+      <h1 style={{ position: "absolute", left: "50%", top: "50%" }}>
+        LOADING...
+      </h1>
+    );
+  } else if (window.visualViewport.width < 800) {
+    return (
+      <main className="history">
+        <Sidebar />
+        <h1>Archive</h1>
+        <div>
+          <div>
+            {" "}
+            <input
+              placeholder="Search by name or tag"
+              value={search}
+              onChange={handleSearch}
+            ></input>{" "}
+          </div>
+          <ul>
+            <CreateList
+              {...data}
+              search={search}
+              status="done"
+              update={"toggleUpdate"}
+            />
+          </ul>
+        </div>
+      </main>
+    );
+  } else {
+    return (
+      <main className="history">
+        <Sidebar />
+        <h1>Archive</h1>
+        <div>
+          <div>
+            {" "}
+            <input
+              placeholder="Search by name or tag"
+              value={search}
+              onChange={handleSearch}
+            ></input>{" "}
+          </div>
+          <div className="list-container">
+            <CreateList
+              {...data}
+              search={search}
+              status="done"
+              update={"toggleUpdate"}
+            />
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
